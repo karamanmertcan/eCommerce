c52b9df05a866c8649cd491b5e699ae392869377
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-08-29 00:31:59 +0300

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>Welcome To Proshop</main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
