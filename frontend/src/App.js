8d2a13633a244fc58635726a775f66d8b97059be
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-08-29 13:33:35 +0300

    backend kuruldu , product ekrani , home ekrani , header, footer yapildi.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main>Welcome To Proshop</main>
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
+        </Container>
+      </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
